6bbed5cb251bddccc216e9129a8807ea4ad9e21d
gelvazio <gelvazio@hotmail.com>
2020-01-22 13:00:35 -0300

    INICIAL

---
+++ node_sequalize/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect : 'postgres',
+  host    : 'salt.db.elephantsql.com',
+  username: 'ftgcyhzw',
+  password: 'GeiysUXytMjx_UY-PKxlIgYuLLY7ck6u',
+  database: 'ftgcyhzw',
+  define  : {
+    timestamps: true,
+    underscored: true,
+  },
+};
